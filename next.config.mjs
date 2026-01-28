ee65d12143596077dd2d95164b274a6bc1e0e0fe
kamelpjlk <127057764+kamelpjlk@users.noreply.github.com>
2026-01-28 23:13:09 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/ydslien',
+  assetPrefix: '/ydslien',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
