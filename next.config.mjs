9891dd9effa505f5b118d04741c3a5ba5c43e827
Abdarrazik Kamel <kamel.abdarrazik@gmail.com>
2026-01-29 13:29:37 +0100

    Fix merge conflict in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   output: 'export',
   basePath: '/ydslien',
   assetPrefix: '/ydslien',
@@ -10,14 +9,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-=======
-    output: 'export',
-    basePath: '/ydslien',
-    assetPrefix: '/ydslien',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
->>>>>>> a1bc9cd (Updates: opaque cards, logo z-index on drag, visible icons, phone card, violet website card)
